a04a4b2f682edc8a9c97de41cf0366761b996cf5
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-07 21:54:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker name
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+  names.map { |n| badge_maker(n) }
+end
+
+def assign_rooms names
+  output = []
+  names.each_with_index {|index, val| output << "Hello, #{val}! You'll be assigned to room #{index}!"}
+  output
+end
